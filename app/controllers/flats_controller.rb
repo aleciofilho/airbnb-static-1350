2bfc5483d07f37013b68d0a898da6364e4543c8f
Alecio Filho <alecio_filho@hotmail.com>
2023-08-21 19:01:30 -0300

    all pages and css

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,20 @@
+require "open-uri"
+require "json"
+
+class FlatsController < ApplicationController
+  def index
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(URI.open(url).read)
+  end
+  
+  def show
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    flats = JSON.parse(URI.open(url).read)
+
+    @flatizinho = flats.select do |flat| 
+      flat["id"] == params[:id].to_i 
+    end.first
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'flats/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # get "/" , to: "flats#index"
+  root "flats#index"
+
+  get "/flats/:id", to: "flats#show", as: :flat
+  # get "/flats/apply", to: "flats#apply"
+
 end
